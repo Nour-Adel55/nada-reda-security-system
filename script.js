b3244da5006cf0fb2d26a889b72c96ed025b922c
Nour-adel <nouradel1682008@gmail.com>
2024-11-25 07:51:18 -0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+let menuItems = [
+        {
+        name: "hujiik",
+        price: "56:00$",
+        description: "Tapo TP-Link Pan/Tilt Security Camera for Baby Monitor, Pet Camera w/Motion Detection, 1080P, 2-Way Audio, Night Vision, Cloud & SD Card Storage, Works with Alexa & Google Home ( C200)",
+        image:"img/WhatsApp Image 2024-11-25 at 09.43.15_f70c3162.jpg"
+
+},
+{
+        name: "hujiik",
+        price: "56:00$",
+        description: "Nikon d850 with af-s 24-120mm f/4 g ed vr lens kit",
+        image:"img/WhatsApp Image 2024-11-25 at 09.43.12_754ca5cf.jpg"
+
+},
+{
+        name: "hujiik",
+        price: "56:00$",
+        description: "ELTERAZONE Baby Monitor Pet Camera Monitors WiFi Camera Mini Camera A9 Security Camera Wireless Surveillance HD 1080P Night Vision for Home ",
+        image:"img/WhatsApp Image 2024-11-25 at 09.43.12_754ca5cf.jpg"
+
+
+}
+
+
+]
